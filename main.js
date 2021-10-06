de5f614c0ce5cb0d650ee5482996fbf0488490a0
VRJVOID <sanketshirsath226@gmail.com>
2021-10-06 10:52:38 +0530

    Newly added search bar and fix a result bug

--- main.js
+++ main.js
@@ -1,42 +1,10 @@
-const subButton=document.getElementById("sub")
-const responseText=document.getElementById("res")
-//let res=true
-
-/*responseText.innerText="Results will appear here"
-subButton.onclick=function(){
-    responseText.innerText="Loading..."
-    fetch( 'https://phishshield.herokuapp.com/post?URL=https://www.faceb0ok.com/' )
-    .then( response => response.json() )
-    .then( response => {
-        // Do something with response.
-        //res=response
-        console.log(response)
-        
-        if(response==1){
-            responseText.innerText="Safe Website"
-        }
-        else if(response==0){
-            responseText.innerText="Not recognized"
-        }
-        else if(response==2){
-            responseText.innerText="Loading..."
-        }
-        else{
-            responseText.innerText="Phishing website"
-        }
-    } );*/
-    const clearInput = () => {
-        const input = document.getElementsByTagName("input")[0];
-        input.value = "";
-      }
-      
-      const clearBtn = document.getElementById("clear-btn");
-      clearBtn.addEventListener("click", clearInput);
-    
-    
-
-
-//}
+// const subButton=document.getElementsByClassName("search-btn")[0]
+// const responseText=document.getElementById("res")
+// subButton.addEventListener("click",
+// function()
+// {
+//     console.log("hello")
+// })
 
 
 
