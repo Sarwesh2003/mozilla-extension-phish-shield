4d0152f32d42a486a53967e8cfa253b2da20a6eb
Sarwesh2003 <sarweshkhairnar@gmail.com>
2021-10-07 10:35:19 +0530

    Backed Connected

--- main.js
+++ main.js
@@ -1,41 +1,62 @@
 const subButton=document.getElementById("sub")
 const responseText=document.getElementById("res")
 const backbtn=document.getElementById("back")
+const bg=document.getElementsByTagName("body")[0]
+const urlText=document.getElementById("search")
+const input=document.getElementById("inputform")
 //let res=true
-subButton.onclick=function(){
+/*subButton.onclick=function(){
     responseText.innerText="Hello"
-    alert("Click")
-}
+    bg.className="blink-bg-yellow"
+    let temp=document.getElementById("search").value
+    console.log(temp)
+}*/
 
 
-/*responseText.innerText="Results will appear here"
-subButton.onclick=function(){
+
+//input.addEventListener("submit",(event))
+
+function showResult(){
     responseText.innerText="Loading..."
-    fetch( 'https://phishshield.herokuapp.com/post?URL=https://www.faceb0ok.com/' )
+    let url=urlText.value
+    fetch( 'https://phishshield.herokuapp.com/post?URL='+url )
     .then( response => response.json() )
     .then( response => {
-        // Do something with response.
-        //res=response
-        console.log(response)
-        
         if(response==1){
-            responseText.innerText="Safe Website"
-        }
-        else if(response==0){
-            responseText.innerText="Not recognized"
-        }
-        else if(response==2){
-            responseText.innerText="Loading..."
-        }
-        else{
-            responseText.innerText="Phishing website"
-        }
-    } );*/
-   
-   
+        responseText.innerHTML="<b>Safe Website</b>"
+        bg.className="blink-bg-green"
+    }
+    else if(response==0){
+        responseText.innerText="Not recognized"
+        bg.className="blink-bg-yellow"
+    }
+    else if(response==2){
+        responseText.innerText="Loading..."
+    }
+    else{
+        responseText.innerText="Phishing website"
+        bg.className="blink-bg-red"
+    }
+    urlText.value=""
+        
+    } );
+    
+}
+
+responseText.innerText="Results will appear here"
+urlText.addEventListener("keydown",e=>{
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        // Trigger the button element with a click
+        showResult()
+      }
+})
 
 
-//}
+subButton.onclick=function(){
+    showResult()
+    bg.className=""
+}
 
 
 
